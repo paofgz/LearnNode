c8851e027606129e51de3a6640d3008ce5171c09
paofgz <paofgz2000@gmail.com>
2021-07-05 14:52:00 -0500

    File upload

---
+++ Natours API/routes/tourRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const tourController = require('./../controllers/tourControllers');
+
+const router = express.Router();
+
+// router.param('id', tourController.checkID);
+
+// Create a checkbody middleware
+// Check if body contains the name and price property
+// If not, send back 400 (bad request)
+// Add it to the post handler stack
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createTour);
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+module.exports = router;
