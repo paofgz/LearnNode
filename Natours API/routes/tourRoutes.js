2f92ffc5506c9ca6cea62a422cff5cb61eafbc88
paofgz <paofgz2000@gmail.com>
2021-07-06 11:19:12 -0500

    queries

--- Natours API/routes/tourRoutes.js
+++ Natours API/routes/tourRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const tourController = require('../controllers/tourControllers');
+const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
+
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
 // Create a checkbody middleware
 // Check if body contains the name and price property
