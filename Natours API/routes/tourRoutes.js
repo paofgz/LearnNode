a98ded4e74ba0285b7edf10901b4aa2d8eb8f97e
paofgz <paofgz2000@gmail.com>
2021-07-06 12:52:07 -0500

    Aggregation

--- Natours API/routes/tourRoutes.js
+++ Natours API/routes/tourRoutes.js
@@ -9,10 +9,8 @@ router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
-// Create a checkbody middleware
-// Check if body contains the name and price property
-// If not, send back 400 (bad request)
-// Add it to the post handler stack
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/')
